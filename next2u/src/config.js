90bae304a1f2db7cc24d1363d9162a970fb18c96
aina_valls <avallsru@gmail.com>
2021-04-24 17:47:33 +0200

    setting up the app and the firebase config

---
+++ next2u/src/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDZJ_oShWxFfeYy_61x68deySvIQ8Oqa4A",
+  authDomain: "next2u-31408.firebaseapp.com",
+  projectId: "next2u-31408",
+  storageBucket: "next2u-31408.appspot.com",
+  messagingSenderId: "1022010436240",
+  appId: "1:1022010436240:web:b4b6a097c068d2ad844615"
+};
+
+export default firebaseConfig;
