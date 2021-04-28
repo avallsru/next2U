7b6b5e2a101731079f5cf0145485e0a24422f04d
aina_valls <avallsru@gmail.com>
2021-04-28 21:00:02 +0200

    creating the imgs for the store categories and a fake BBDD for the store categories

---
+++ next2u/src/bbddFake/storesCategoriesBBDD.js
@@ -0,0 +1,33 @@
+export const storesCatBBDD = [
+  {
+    id: 1,
+    name: 'Alimentación',
+    img: 'stores_categories/1.jpg'
+  },
+  {
+    id: 2,
+    name: 'Dulces',
+    img: 'stores_categories/2.jpg'
+  },
+  {
+    id: 3,
+    name: 'Niños',
+    img: 'stores_categories/3.jpg'
+  },
+  {
+    id: 4,
+    name: 'Adultos',
+    img: 'stores_categories/4.jpg'
+  },
+  {
+    id: 5,
+    name: 'Ropa',
+    img: 'stores_categories/5.jpg'
+  },
+  {
+    id: 6,
+    name: 'Cuidado personal',
+    img: 'stores_categories/6.jpg'
+  },
+
+]
