c1032f78a397b74cc71a7c103d2b255fdfeb34b4
aina_valls <avallsru@gmail.com>
2021-05-01 11:11:57 +0200

    one card store ui defined

--- next2u/src/Components/StoreCard/StoreCard.js
+++ next2u/src/Components/StoreCard/StoreCard.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React from "react";
 // import PropTypes from 'prop-types';
 
-
 import { useSelector } from "react-redux";
+import { FcClock, FcMoneyTransfer } from "react-icons/fc";
 
-import { storesCatBBDD } from '../../bbddFake/storesCategoriesBBDD';
+import { storesCatBBDD } from "../../bbddFake/storesCategoriesBBDD";
 
-import './StoreCard.scss'
+import "./StoreCard.scss";
 
-const StoreCard = store => {
-
+const StoreCard = (store) => {
   //to delete after the storecardlist is implemented
-  const storesSelected = useSelector((store) => store.storesReducer.storesNearAddress);
-  const temporalStore = storesSelected[0]
+  const storesSelected = useSelector(
+    (store) => store.storesReducer.storesNearAddress
+  );
+  const temporalStore = storesSelected[0];
   ////
 
   //to change when firebase bbdd where implemented
@@ -21,37 +22,50 @@ const StoreCard = store => {
   /////
 
   const getCatNames = (categoriesArr) => {
-    const categoriesName = []
-    categoriesArr.forEach(idToFind => {
-      const filteredByCat = categories.filter(category => category.id === idToFind)
-      filteredByCat.forEach(categoryObj => {
+    const categoriesName = [];
+    categoriesArr.forEach((idToFind) => {
+      const filteredByCat = categories.filter(
+        (category) => category.id === idToFind
+      );
+      filteredByCat.forEach((categoryObj) => {
         categoriesName.push(categoryObj.name);
-      })
-    })
+      });
+    });
 
-    return categoriesName.join(' ')
-
-  }
+    return categoriesName.join(" ");
+  };
   return (
     <div className="storeCard-container">
-      <div className="img-container">
-        <img src="assets/img/stores/1.jpg" alt="store-logo"/>
+      <div className="store-img container">
+        <img src="assets/img/stores/1.jpg" alt="store-logo" />
       </div>
-      <div className="store-basic-data">
+      <div className="store-basic-data container">
         <p className="store-name">{temporalStore.name}</p>
-        <div className="category-names">
-          {getCatNames(idsToSearch)}
-        </div>
+        <div className="category-names">{getCatNames(idsToSearch)}</div>
       </div>
-      <div className="store-other-details">
+      <div className="store-other-details container">
         <div className="opening-hours">
-          {temporalStore['opening_hours']}
+          <FcClock />
+          {temporalStore["opening_hours"]}
         </div>
-        <div className="delivery-price">
-          {`Delivery price: ${temporalStore["deliver_price"]}€`}
-        </div>
-        <div className="minimum-order">
-          {`Min. order: ${temporalStore["minimum_price_order"]}€`}
+        <div className="paying-info">
+          <FcMoneyTransfer />
+          <div className="delivery-price">
+            <div className="delivery-price">
+              <span>
+                <b>Entrega:</b>
+              </span>
+              <span>{`${temporalStore["deliver_price"]}€`}</span>
+            </div>
+          </div>
+          <div className="minimum-order">
+            <div className="minimum-order">
+              <span>
+                <b>Pedido mín:</b>
+              </span>
+              <span>{`${temporalStore["minimum_price_order"]}€`}</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -59,7 +73,7 @@ const StoreCard = store => {
 };
 
 // StoreCard.propTypes = {
-  
+
 // };
 
 export default StoreCard;
