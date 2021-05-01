e50e0629e9e633b4db7906dcc3bcf29d609bc60a
aina_valls <avallsru@gmail.com>
2021-05-01 11:26:38 +0200

    first styles of the results page

--- next2u/src/containers/StoresResults.js
+++ next2u/src/containers/StoresResults.js
@@ -6,15 +6,23 @@ import StoreCatList from '../Components/StoreCatList';
 import InputRestName from '../Components/InputRestName';
 import StoreCard from '../Components/StoreCard/StoreCard';
 
+import './StoresResults.scss';
+
 import { storesCatBBDD } from '../bbddFake/storesCategoriesBBDD';
 
 const StoresResults = props => {
-  const storeToPrint = storesCatBBDD[0];
+  // const storeToPrint = storesCatBBDD[0];
   return (
     <div>
-      <StoreCatList />
-      <InputRestName />
-      <StoreCard />
+      <div className="filters-container">
+        <StoreCatList className="filter-by-cat" />
+        <InputRestName className="filter-by-name" />
+      </div>
+      <div className="stores-container">
+        <StoreCard />
+      </div>
+      
+      
     </div>
   );
 };
