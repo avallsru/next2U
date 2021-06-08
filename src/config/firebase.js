308a46e9869ca39ba48062e47c9597b0e620023f
aina_valls <avallsru@gmail.com>
2021-06-08 21:20:18 +0200

    trying to resolve firebase problems

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "next2u-31408.firebaseapp.com",
+  databaseURL: "https://next2u-31408-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "next2u-31408",
   storageBucket: "next2u-31408.appspot.com",
   messagingSenderId: "1022010436240",
